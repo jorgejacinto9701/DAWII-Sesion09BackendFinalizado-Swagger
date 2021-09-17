12efb6dd2c8cb730c9840b96804a08f393ed4f38
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-09-17 17:27:15 -0500

    Ajutes en Ubigeo

--- src/main/java/com/empresa/repository/UbigeoRepository.java
+++ src/main/java/com/empresa/repository/UbigeoRepository.java
@@ -11,13 +11,13 @@ import com.empresa.entity.Ubigeo;
 public interface UbigeoRepository extends JpaRepository<Ubigeo, Integer>{
 
 	
-	@Query("select distinct x.departamento from Ubigeo x")
+	@Query("select distinct x.departamento from Ubigeo x order by 1 asc")
 	public abstract List<String> listaDepartamentos();
 	
-	@Query("select distinct x.provincia from Ubigeo x where x.departamento = :var_dep")
+	@Query("select distinct x.provincia from Ubigeo x where x.departamento = :var_dep  order by 1 asc")
 	public abstract List<String> listaProvincias(@Param("var_dep") String departamento);  
 
-	@Query("select x from Ubigeo x where x.departamento = :var_dep and x.provincia = :var_pro")
+	@Query("select x from Ubigeo x where x.departamento = :var_dep and x.provincia = :var_pro  order by x.distrito asc")
 	public abstract List<Ubigeo> listaDistritos(@Param("var_dep") String departamento,@Param("var_pro") String provincia);
 	
 	
